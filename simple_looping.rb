8f9e24cd031d8b19dd01086f17d44f91367b9f5a
Sean Sutton Jr <sutton_mjbc@icloud.com>
2018-09-01 00:51:12 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,6 +1,11 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
+  counter = 0
+  until counter == 7
+  puts phrase
+  counter += 1
+  
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
   # The integer is the number of times the loops should puts out the phrase
