375ce9adab60483051b747c90705cb4a055614a3
Sean Sutton Jr <sutton_mjbc@icloud.com>
2018-09-01 01:03:56 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -20,7 +20,7 @@ end
 def while_iterator(number_of_times)
   number_of_times = 0
   
-  while number_of_times <= 7
+  while number_of_times < 7
     puts "Welcome to Flatiron School's Web Development Course!"
   number_of_times += 1 
   end 
